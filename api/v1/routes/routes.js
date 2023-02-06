d64e512b99c5c880a434ede8d75b5ceb9124d8c0
Ghelonico Maligaya <ghelonicomaligaya111@gmail.com>
2023-02-06 22:46:13 +0800

    need to add token function

--- api/v1/routes/routes.js
+++ api/v1/routes/routes.js
@@ -4,11 +4,13 @@ import {
     createUser,
 } from "../controllers/userCont.js";
 
+import { loginUser } from "../controllers/authCont.js";
+
 const router = express.Router();
 //users
 router.get("/users", showUsers);
 router.post("/user", createUser);
-// router.post('/login', loginUser);
+router.post('/login', loginUser);
 // router.post('/protected', validateToken);
 
 export default router;
